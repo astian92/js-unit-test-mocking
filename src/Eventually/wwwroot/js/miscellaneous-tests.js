4ecaa6bcd92a4890540061c65c59cf2cd477b353
astian <m.abrashev@vripack.com>
2016-11-17 14:12:58 +0200

    second commit

---
+++ src/Eventually/wwwroot/js/miscellaneous-tests.js
@@ -0,0 +1,294 @@
+﻿
+mocha.setup('bdd');
+expect = chai.expect;
+chai.should();
+
+describe("#ajax - Ajax extensions I have written to simplify ajax calls", function () {
+    it("get Test", function () {
+        let url = "http://Test/get";
+        let settings = { conf1: 1, conf2: false };
+        let finalArg = { type: "GET", url: url, data: undefined, conf1: settings.conf1, conf2: settings.conf2 };
+
+        let ajaxStub = sinon.stub($, "ajax", function (arg) {
+            return arg;
+        });
+
+        let expected = Ajax.get(url, settings);
+
+        //test that it was called only once
+        expect(ajaxStub.calledOnce).to.be.true;
+
+        //test that the arguments were assembled correctly
+        let argumentsWerePropagated = ajaxStub.calledWith(finalArg);
+        expect(argumentsWerePropagated).to.be.true;
+
+        //test if it propagates the result from the stub back to the caller (usually it should be a promise but I stubbed it to return the param itsel);
+        expect(expected).to.eql(finalArg);
+
+        $.ajax.restore();
+    });
+
+    it("post Test", function () {
+        let url = "http://Test/post";
+        let settings = { conf1: 1, conf2: false };
+        let data = { name: "Martin", age: 24 };
+        let finalArg = { type: "POST", url: url, data: data, conf1: settings.conf1, conf2: settings.conf2 };
+
+        let ajaxStub = sinon.stub($, "ajax", function (arg) {
+            return arg;
+        });
+
+        let expected = Ajax.post(url, data, settings);
+
+        //test it was called only once
+        expect(ajaxStub.calledOnce).to.be.true;
+
+        //test that the arguments were correctly assembled
+        let argumentsWerePropagated = ajaxStub.calledWith(finalArg);
+        expect(argumentsWerePropagated).to.be.true;
+
+        //test if it propagets the result
+        expect(expected).to.eql(finalArg);
+
+        $.ajax.restore();
+    });
+
+    //it("getAndHandle Test", function () {
+    //    //window.InfoNav = { renderResponse: function () { } };
+    //    //let infoNavSpy = sinon.stub(InfoNav, "renderResponse");
+
+    //    //let ticket = { Event: "event" };
+        
+    //    //let promise = sinon.stub($, "ajax")
+    //    //    .resolves(ticket);
+
+    //    //let url = "http://Test/getAndHandle";
+    //    //let settings = { conf1: 1, conf2: false };
+    //    //let finalArg = { type: "GET", url: url, data: undefined, conf1: settings.conf1, conf2: settings.conf2 };
+
+    //    ////Ajax.getAndHandle(url, settings);
+
+    //    ////let propagated = promise.calledWith(finalArg);
+
+    //    ////expect(propagated).to.be.true;
+
+    //    //infoNavSpy.restore();
+    //    //$.ajax.restore();
+    //});
+    it("testing stubs using done", function (done) {
+        let returnsPromise = sinon.stub($, "ajax").resolves('success');
+
+        returnsPromise().then(function (result) {
+            expect(result).to.equal('success');
+            $.ajax.restore();
+            done();
+        }, function (error) {
+            console.log(error);
+            $.ajax.restore();
+            done();
+        })
+    });
+
+    it('testing eventually', function () {
+        let returnsPromise = sinon.stub($, "ajax").resolves('success');
+        return expect(returnsPromise()).should.eventually.equal('success');
+        //return expect(returnsPromise).should.eventually.equal('success');
+    });
+
+});
+
+
+describe('#misc', function () {
+    describe("RoundNumber(Number, Digits) - rounds up a number up to the number of the digit selected", function () {
+        it('RoundNumber Test', function () {
+            let input = 4.123456789;
+            let expected = 4.13;
+
+            let actual = RoundNumber(input, 2);
+            expect(actual).to.equal(expected);
+        });
+        it('RoundNumber Test with 0 digits', function () {
+            let input = 1.234567;
+            let expected = 2;
+
+            let actual = RoundNumber(input, 0);
+            expect(actual).to.equal(expected);
+        });
+        it('RoundNumber Test with negative digits', function () {
+            let call = function () {
+                RoundNumber(1.234, -5);
+            }
+
+            expect(call).to.throw();
+        });
+        it('RoundNumber Test with incorrect number', function () {
+            let call = function () {
+                RoundNumber("asd", 3);
+            }
+
+            expect(call).to.throw();
+        });
+        it('RoundNumber Test with incorrect digits', function () {
+            let call = function () {
+                RoundNumbeR(5.654, {});
+            }
+
+            expect(call).to.throw();
+        });
+    });
+    
+    describe("guid() - returns a string value that represents a valid UID", function () {
+        it("guid Test", function () {
+            let actual = guid();
+            let regex = /[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}/i;
+            let match = regex.test(actual);
+
+            expect(match).to.be.true;
+        })
+    })
+
+    describe("Number.pad(size) - extension. Returns a string of the number with number of zeroes infront of it equal to size", function () {
+        it("pad Test with size = 5", function () {
+            let number = 3;
+            let actual = number.pad(5);
+            let expected = "00003";
+
+            expect(actual).to.equal(expected);
+        });
+        it("pad Test with no pads", function () {
+            let call = function () { 
+                let number = 3;
+                number.pad();
+            }
+
+            expect(call).to.throw();
+        });
+        it("pad Test with negative size", function () {
+            let call = function () {
+                let number = 3;
+                number.pad(-3);
+            }
+
+            expect(call).to.throw();
+        });
+        it("pad Test with size = 0", function () {
+            let number = 71;
+            let actual = number.pad(0);
+
+            let expected = number.toString();
+        });
+    });
+
+    describe("css(jqueryElement) - returns a FULL specification of every css rule applied to the element! COMING FROM STYLESHEET!", function () {
+        it("css Test with this div#mocha - testing if its style to have 20px margin is detected by this method", function () {
+            let element = $('#mocha');
+            let elementCss = css(element);
+
+            let actual = elementCss["margin"];
+            expect(actual).to.equal("20px");
+        });
+        it("css Test without parameters", function () {
+            let call = function () {
+                css();
+            }
+
+            expect(call).to.throw();
+        });
+        it("css Test with no element", function () {
+            let call = function () {
+                css($('.somethingwhichdoesntexist'));
+            }
+
+            expect(call).to.throw();
+        });
+    });
+
+    describe("getCaretPosition(ctrl) - returns a integer showing the caret position in teh ", function () {
+        it("getCaretPosition Test", function () {
+            let input = $('<input type="text" />');
+            input.val('text'); //text = 4 characters 
+            let expected = 4;
+            let actual = getCaretPosition(input[0]);
+
+            expect(actual).to.equal(expected);
+        });
+        it("getCaretPosition Test - input no text", function () {
+            let input = $('<input type="text" />');
+            let expected = 0;
+            let actual = getCaretPosition(input[0]);
+
+            expect(actual).to.equal(expected);
+        });
+    });
+
+    describe("setCaretPosition(ctrl, pos) - sets the cursor(caret) at a certain position. *CHECKING through getCaretPosition to assert... no other way to check result", function () {
+        it("setCaretPosition Test", function () {
+            let input = $('<input type="text" />');
+            input.val('text is longer than last time');
+
+            let expected = 7;
+            setCaretPosition(input[0], expected);
+
+            let actual = getCaretPosition(input[0]); //no other way to check ... that I can think of
+            expect(actual).to.equal(expected);
+        });
+        it("setCaretPosition Test - no text in input", function () {
+            let input = $('<input type="text" />');
+            let expected = 0;
+            setCaretPosition(input[0], 7);
+
+            let actual = getCaretPosition(input[0]);
+            expect(actual).to.equal(0);
+        });
+    });
+
+    describe("toHHMM(intMinutes) - Changes minutes (as integer) to hh:mm format", function () {
+        it("toHHMM Test", function () {
+            let hours = 750;
+            let expected = "12:30";
+            let actual = toHHMM(hours);
+
+            expect(actual).to.equal(expected);
+        });
+        it("toHHMM Test - negative intMinutes", function () {
+            let hours = -500;
+            let expected = "-08:20";
+            let actual = toHHMM(hours);
+
+            expect(actual).to.equal(expected);
+        });
+        it("toHHMM Test - 0 intMinutes", function () {
+            let hours = 0;
+            let expected = "00:00";
+            let actual = toHHMM(hours);
+
+            expect(actual).to.equal(expected);
+        });
+    });
+
+    describe("toIntMinutes(hhMM) - Changes hh:mm to minutes (as int)", function () {
+        it("toIntMuntes Test", function () {
+            let input = "12:30";
+            let expected = 750;
+            let actual = toIntMinutes(input);
+
+            expect(actual).to.equal(expected);
+        });
+        it("toIntMinutes Test - negative hours", function () {
+            let input = "-09:15";
+            let expected = -555;
+            let actual = toIntMinutes(input);
+
+            expect(actual).to.equal(expected);
+        });
+        it("toIntMinutes Test - \"00:00\" hhMM", function () {
+            let input = "00:00";
+            let expected = 0;
+            let actual = toIntMinutes(input);
+
+            expect(actual).to.equal(expected);
+        });
+    });
+});
+
+mocha.run();
---
+++ src/Eventually/wwwroot/js/miscellaneous.js
@@ -0,0 +1,204 @@
+﻿
+//IMPORTANT - ajax calls simplified
+var Ajax = (function () {
+    var ajaxCall = function (type, url, data, settings) {
+
+        var ajaxObject = {
+            type: type,
+            url: url,
+            data: data || undefined
+        };
+
+        for (var propertyName in settings) {
+            ajaxObject[propertyName] = settings[propertyName];
+        }
+
+        return $.ajax(ajaxObject);
+    }
+
+    var ajaxHandledCall = function (type, url, data, settings) {
+        var promise = ajaxCall(type, url, data, settings);
+
+        promise.done(function (ticket) {
+            if (ticket.Event != undefined && ticket.Event != null && ticket.Event != "") {
+                InfoNav.renderResponse(ticket);
+            }
+        }).fail(function (error) {
+            InfoNav.addError({ Message: "There was an unexpected error! Please check if your network connection is ok." });
+        });
+
+        return promise;
+    }
+
+    var get = function (url, settings) {
+        return ajaxCall('GET', url, undefined, settings);
+    }
+
+    var post = function (url, data, settings) {
+        return ajaxCall('POST', url, data, settings);
+    }
+
+    var getAndHandle = function (url, settings) {
+        return ajaxHandledCall('GET', url, undefined, settings);
+    }
+
+    var postAndHandle = function (url, data, settings) {
+        return ajaxHandledCall('POST', url, data, settings);
+    }
+
+    return {
+        get: get,
+        post: post,
+        getAndHandle: getAndHandle,
+        postAndHandle: postAndHandle
+    }
+}());
+
+function RoundNumber(number, digits) {
+    if (typeof number != "number") {
+        throw "Number argument has to be a number!";
+    }
+
+    if (typeof digits != "number") {
+        throw "Digits argument has to be an number!"
+    }
+
+    if (digits < 0) {
+        throw "Digits cannot be a negative number!";
+    }
+
+    var gear = Math.pow(10, digits);
+
+    var up = number * gear;
+    var ceiled = Math.ceil(up);
+    var down = ceiled / gear;
+
+    return down;
+    
+    //return Math.ceil(number * gear) / gear;
+}
+
+var guid = (function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+                   .toString(16)
+                   .substring(1);
+    }
+    return function () {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+               s4() + '-' + s4() + s4() + s4();
+    };
+})();
+
+Number.prototype.pad = function (size) {
+    if (!size && size != 0) {
+        throw "Size of padd must be a number!";
+    }
+
+    if (size < 0) {
+        throw "size of padd must be a positive number!";
+    }
+
+    var s = String(this);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+}
+
+//--1
+//returns FULL specification of every css property applied for the given object (only from stylesheets!)
+function css(jqueryElement) {
+    if (!jqueryElement || jqueryElement.length == 0) {
+        throw "Element is missing!";
+    }
+
+    var sheets = document.styleSheets;
+    var result = {};
+
+    for (var i in sheets) {
+        var rules = sheets[i].rules || sheets[i].cssRules;
+
+        for (var rule in rules) {
+            if (jqueryElement.is(rules[rule].selectorText)) { //matches to css selector (true)
+                //result = $.extend(result, css2json(rules[rule].style), css2json(jqueryElement.attr('style')));
+                result = $.extend(result, rules[rule].style, jqueryElement.attr('style'));
+            }
+        }
+    }
+
+    return result;
+}
+
+//gets the position of the cursor(caret) in an input field. ctrl must be a simple element (not jquery element)
+function getCaretPosition(ctrl) {
+    var CaretPos = 0;
+    // IE Support
+    if (document.selection) {
+        ctrl.focus();
+        var Sel = document.selection.createRange();
+        Sel.moveStart('character', -ctrl.value.length);
+        CaretPos = Sel.text.length;
+    }
+    // Firefox support
+    else if (ctrl.selectionStart || ctrl.selectionStart == '0') {
+        CaretPos = ctrl.selectionStart;
+    }
+
+    return (CaretPos);
+}
+
+function setCaretPosition(ctrl, pos) {
+    if (ctrl.setSelectionRange) {
+        ctrl.focus();
+        ctrl.setSelectionRange(pos, pos);
+    }
+    else if (ctrl.createTextRange) {
+        var range = ctrl.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+    }
+}
+
+//Changes minutes (as integer) to hh:mm format
+function toHHMM(intMinutes) {
+    var sign = "";
+    if (intMinutes < 0) {
+        sign = '-';
+        intMinutes *= -1;
+    }
+    var minutes = intMinutes % 60;
+    var hours = (intMinutes - minutes) / 60;
+    //var minutes = (((decimalHour * 100) % 100) / 100) * 60;
+    if (hours.length == 1) {
+        hours = '0' + hours;
+    }
+    if (minutes == '0') {
+        minutes = '00';
+    }
+    if (minutes < 0) {
+        minutes = minutes + 60;
+        hours++;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    return sign + hours + ":" + minutes;
+}
+
+//Changes from hh:mm to minutes (as int)
+function toIntMinutes(hhMM) {
+    var isMinus = 1;
+    if (hhMM[0] == "-") {
+        isMinus = -1;
+    }
+
+    if (hhMM.indexOf(':') == -1) {
+        return 0;
+    }
+    var items = hhMM.split(":");
+    var hours = parseInt(items[0]) * 60;
+    var minutes = items[1];
+
+    return isMinus * (Math.abs(parseInt(hours)) + parseInt(minutes));
+}
